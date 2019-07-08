e1e16c19a43bb73a5537d629aff379a6cb8ef0b3
supagg <aggarwal.supriya15@gmail.com>
2019-07-08 15:10:30 +0530

    commit changes with test case

---
+++ UnitTestofConsoleApp/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestofConsoleApp
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int testMember = 1;
+            Assert.AreEqual(1, testMember);
+        }
+    }
+}
